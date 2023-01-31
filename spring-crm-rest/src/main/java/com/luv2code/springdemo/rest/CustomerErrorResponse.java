7d1cb62fcd6b87501b53f2270c15b222ec4c3ebd
JorgeRomero01 <jrome140@fiu.edu>
2023-01-30 22:14:54 -0500

    exception handling

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerErrorResponse.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerErrorResponse.java
@@ -5,6 +5,16 @@ public class CustomerErrorResponse {
 	private String message;
 	private long timeStamp;
 	
+	public CustomerErrorResponse() {
+		
+	}
+
+	public CustomerErrorResponse(int status, String message, long timeStamp) {
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+	
 	
 	
 }
