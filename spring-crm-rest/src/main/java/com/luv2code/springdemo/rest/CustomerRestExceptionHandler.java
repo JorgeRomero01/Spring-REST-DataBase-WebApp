0ab347f158f96bc099b9013106f750c508903a38
JorgeRomero01 <jrome140@fiu.edu>
2023-01-31 18:35:42 -0500

    z

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,7 @@ public class CustomerRestExceptionHandler {
 	@ExceptionHandler
 	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc){
 		//create CustomerErrorResponse
-		CustomerErrorResponse error = new CustomerErrorResponse();
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(), exc.getMessage(), System.currentTimeMillis());
 		
 		//return ResponseEntity
 		return null;
