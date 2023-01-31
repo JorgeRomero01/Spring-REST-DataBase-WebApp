83b8e16d250a016ea8f6ef1f1da6aa0efd69c821
JorgeRomero01 <jrome140@fiu.edu>
2023-01-30 22:43:58 -0500

    GG

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerErrorResponse.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerErrorResponse.java
@@ -15,6 +15,29 @@ public class CustomerErrorResponse {
 		this.timeStamp = timeStamp;
 	}
 	
-	
+	//setters and getters
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 	
 }
